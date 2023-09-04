0c9b3caadbc0b5de70e62ee4055bfaa19c774c4d
Alexandre Martos <contact@amartos.fr>
2023-09-04 14:42:48 +0200

    feat(headers): add opt-out macro for optional modules
    Signed-off-by: Alexandre Martos <contact@amartos.fr>

--- include/sccroll.h
+++ include/sccroll.h
@@ -21,7 +21,23 @@
 #include "sccroll/helpers.h"
 #include "sccroll/lists.h"
 #include "sccroll/data.h"
+
+/**
+ * The following are optional features. They do not impact the units
+ * tests if none of their defined macros/functions are used.
+ *
+ * You can disable their inclusion by defining the macros:
+ * - #SCC_NOASSERT for both (since the mocks module depends on the
+ *   assert module; this will change in a future release to be able to
+ *   use them independently)
+ * - #SCC_NOMOCKS for the mocks module
+ */
+#ifndef SCC_NOASSERT
 #include "sccroll/assert.h"
+
+#ifndef SCC_NOMOCKS
 #include "sccroll/mocks.h"
 
+#endif // SCC_NOMOCKS
+#endif // SCC_NOASSERT
 #endif // SCCROLL_H_
